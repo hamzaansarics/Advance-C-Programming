59cc01129e594a3ef82c80d7a5721bad70cf511c
Hamza Ansari <57999738+hamzaansarics@users.noreply.github.com>
2023-01-24 18:37:36 +0000

    While Loop
    Simply Download this file and Open it in your loving Platform

---
+++ While inside While/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Basic_Writing_Structure
+{
+    class program{
+        static void Main()
+        {            
+            bool main=true;
+            while(main){
+                bool inner=true;
+                while(inner){
+                Console.Clear();
+                 int num=int.Parse(Console.ReadLine());
+                if(num==1){
+                    Console.WriteLine("Put Your Love Nubmer");
+                    int nice=int.Parse(Console.ReadLine());
+                    if(nice==00){
+                        inner=false;
+                    }else{
+                        inner=false;
+                        main=false;
+                    }
+                }
+                }
+            }
+        }
+       
+    }
+}
